d01520ce871af45a028ee4a03bc12501f73c09a8
abhijit <abhijitnyk001@gmail.com>
2024-02-01 17:55:45 +0530

    adding login again

--- app.py
+++ app.py
@@ -5,6 +5,11 @@ app = Flask(__name__)
 @app.route("/")
 def helloWorld():
     return "hello world"
-    
+
+
+@app.route("/login")
+def login():
+    return "no login option is available at the moment!"
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
